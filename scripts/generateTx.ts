7718e78dc30718fe8315f40bd7fc850c40ccbc3b
talha <talha.malik@ideofuzion.com>
2023-02-15 00:48:07 +0500

    improved changes

---
+++ scripts/generateTx.ts
@@ -0,0 +1,36 @@
+const ethTx = require('ethereumjs-tx');
+const ethUtils = require('ethereumjs-util');
+import { ethers } from "hardhat";
+
+async function main() {
+    const Deployer = await ethers.getContractFactory(
+        "Deployer"
+      );
+      const deployerBytecode = `${Deployer.bytecode}`;
+    
+    const rawTransaction = {
+        nonce: 0,
+        gasPrice: 100000000000,
+        value: 0,
+        data: deployerBytecode,
+        gasLimit: 247000,
+        v: 27,
+        r: '0x247000',
+        s: '0x2470'
+      }
+    const tx = new ethTx(rawTransaction);
+    const res = {
+        sender: ethUtils.toChecksumAddress(tx.getSenderAddress().toString('hex')),
+        rawTx: '0x' + tx.serialize().toString('hex'),
+        contractAddr: ethUtils.toChecksumAddress(
+          ethUtils.generateAddress(tx.getSenderAddress(), ethUtils.toBuffer(0)).toString('hex')),
+    };
+    console.log('res ', res);
+    
+    return res;
+};
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
